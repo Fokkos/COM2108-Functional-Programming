0d7895025d09ae7b4ebb21409919bb84cc2a7f51
James March <jmarch2@sheffield.ac.uk>
2022-10-18 15:51:28 +0100

    started week 4

--- Bags.hs
+++ Bags.hs
@@ -1,6 +1,16 @@
 module Bags where
+    import Data.List
+
     data Bag a = Bag [a]
 
     listToBag :: [a] -> Bag a
     listToBag xs = Bag xs
 
+    bagEqual :: (Eq a, Ord a) => Bag a -> Bag a -> Bool
+    bagEqual (Bag bagA) (Bag bagB) | length bagA /= length bagB = False
+                                   | bagA == [] && bagB == [] = True
+                                   | elem (head (sort bagA)) (sort bagB) = bagEqual (listToBag (tail (sort bagA))) (listToBag (tail (sort bagB)))
+
+
+    --bagEqual :: (Eq a) => Bag a -> [a]
+    --bagEqual (Bag bagA) = tail bagA
