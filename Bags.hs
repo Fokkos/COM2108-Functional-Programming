94f5c90178595cd8b443c1682e41c451214e1c90
James March <jmarch2@sheffield.ac.uk>
2022-10-20 14:50:32 +0100

    Restarted bags

--- Bags.hs
+++ Bags.hs
@@ -1,6 +1,17 @@
 module Bags where
     import Data.List
 
+    type Item a = (a, Int)
+
+    type Bag a = [Item a]
+
+    listToBag :: Eq a => [a] -> Bag a
+    listToBag (xs) = [(x, i) | x <- xs]
+        where i = 1
+
+    
+
+{-
     data Bag a = Bag [a]
 
     listToBag :: [a] -> Bag a
@@ -9,8 +20,15 @@ module Bags where
     bagEqual :: (Eq a, Ord a) => Bag a -> Bag a -> Bool
     bagEqual (Bag bagA) (Bag bagB) | length bagA /= length bagB = False
                                    | bagA == [] && bagB == [] = True
-                                   | elem (head (sort bagA)) (sort bagB) = bagEqual (listToBag (tail (sort bagA))) (listToBag (tail (sort bagB)))
+                                   | elem (head (bagA)) (bagB) = bagEqual (Bag (tail (bagA))) (Bag (removeFromList [(head (bagA))] bagB))
 
+    removeFromList :: Eq a => [a] -> [a] -> [a]
+    removeFromList _ []           = []
+    removeFromList (y:ys) (x:xs)
+                    | y == x = xs
+                    | otherwise = x : removeFromList [y] xs
 
     --bagEqual :: (Eq a) => Bag a -> [a]
     --bagEqual (Bag bagA) = tail bagA
+-}
+    
